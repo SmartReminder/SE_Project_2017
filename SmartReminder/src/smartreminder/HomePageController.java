51d8b14f641fe546f39f91c4375f744989d8063f
nrbaboo <nrbaboo@hotmail.com>
2017-11-01 23:19:32 +0700

    finish generate day

--- SmartReminder/src/smartreminder/HomePageController.java
+++ SmartReminder/src/smartreminder/HomePageController.java
@@ -5,26 +5,45 @@
  */
 package smartreminder;
 
+import java.awt.Paint;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.DateFormatSymbols;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.scene.Node;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 
 /**
  * FXML Controller class
  *
  * @author 58010622
  */
-public class HomePageController implements Initializable {
 
+public class HomePageController implements Initializable {
+    
     @FXML
     private Rectangle dayBlock1;
     @FXML
@@ -196,98 +215,183 @@ public class HomePageController implements Initializable {
     @FXML
     private ComboBox year_list;
     
-    private static String year;
+    int year = Calendar.getInstance().get(Calendar.YEAR);
     private static int month;  
     ObservableList<String> list = FXCollections.observableArrayList("2017","2018");
     @FXML
     private Button julBtn;
     @FXML
     private Label month_label;
+    @FXML
+    private Label month_label1;
+    @FXML
+    private Label month_label2;
+    @FXML
+    private GridPane calendarPane;
+    private Label[] dayLabel = new Label[42];
+    private Rectangle[] dayBlock = new Rectangle[42];
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO    
-         year_list.setItems(list);  
-        // year_list.setValue("2018");
+        //by chattamet
+         Calendar c = Calendar.getInstance();
+        month = c.get(Calendar.MONTH);
+        String defaultMonth = new DateFormatSymbols().getMonths()[month];
+        generateCalendar(month,year);
+        month_label.setText(defaultMonth);
     }
+    
+    
 
     @FXML
     private void list_Action(ActionEvent event) {
-        year = year_list.getValue().toString();
-        System.out.println(year);
+        year = Integer.parseInt(year_list.getValue().toString());
+        generateCalendar(month,year);
     }
 
     @FXML
     private void janBtn(ActionEvent event) {
-        month = 1;
+        month = 0;
         month_label.setText("January");
+        generateCalendar(month,year);
     }
 
     @FXML
     private void febBtn(ActionEvent event) {
-        month = 2;
+        month = 1;
         month_label.setText("February");
+        generateCalendar(month,year);
     }
 
     @FXML
     private void marBtn(ActionEvent event) {
-        month = 3;
+        month = 2;
         month_label.setText("March"); 
+        generateCalendar(month,year);
     }
 
     @FXML
     private void aprBtn(ActionEvent event) {
-        month = 4;
+        month = 3;
         month_label.setText("April");
+        generateCalendar(month,year);
     }
 
     @FXML
     private void mayBtn(ActionEvent event) {
-        month = 5;
+        month = 4;
         month_label.setText("May");
+        generateCalendar(month,year);
     }
 
     @FXML
     private void junBtn(ActionEvent event) {
-        month = 6;
+        month = 5;
         month_label.setText("June");
+        generateCalendar(month,year);
     }
 
     @FXML
     private void julBtn(ActionEvent event) {
-        month = 7;
+        month = 6;
         month_label.setText("July");
+        generateCalendar(month,year);
     }
     @FXML
     private void augBtn(ActionEvent event) {
-        month = 8;
+        month = 7;
         month_label.setText("August");
+        generateCalendar(month,year);
     }
 
     @FXML
     private void sepBtn(ActionEvent event) {
-        month = 9;
+        month = 8;
         month_label.setText("September");
+        generateCalendar(month,year);
     }
 
     @FXML
     private void octBtn(ActionEvent event) {
-        month = 10;
+        month = 9;
         month_label.setText("October");
+        generateCalendar(month,year);
     }
 
     @FXML
     private void novBtn(ActionEvent event) {
-        month = 11;
+        month = 10;
         month_label.setText("November");
+        generateCalendar(month,year);
     }
 
     @FXML
     private void decBtn(ActionEvent event) {
-        month = 12;
+        month = 11;
         month_label.setText("December");
+        generateCalendar(month,year);
     }
 
+    @FXML
+    private void signOut(MouseEvent event) {
+        SmartReminder.primaryStage.getScene().setRoot(SmartReminder.loginPage);
+    }
+
+   void generateCalendar(int month,int year){
+        int count_day = 1;
+        int rectangle_loop = 1;
+         year_list.setItems(list);  
+         year_list.setValue(year);
+         //month in Calendar class start at 0(0 = january) 
+         Calendar c = Calendar.getInstance();
+         c.set(year, month, 1);
+         int day_of_week = c.get(Calendar.DAY_OF_WEEK);
+         //day of week started at 1 (1 = sunday)
+         
+        // Get the number of days in that month
+         int daysInMonth = c.getActualMaximum(Calendar.DAY_OF_MONTH); 
+         
+         //Get the number of days in that previous month
+         month = (month == 0) ? 11 : month-1;
+         c.set(year,month,1);
+         int daysInPreviousMonth = c.getActualMaximum(Calendar.DAY_OF_MONTH);
+         int day = daysInPreviousMonth+2-day_of_week;
+         int dayinMonth = 1;
+         if(day_of_week == 1){
+             day = 1;           
+         }
+         for (Node child : calendarPane.getChildren()) {
+            if(child.getId()!=null){
+                if(child.getId().contains("dayBlock")){
+                    dayBlock[rectangle_loop-1] = (Rectangle) child;
+                    rectangle_loop++;
+                }
+                if(child.getId().contains("dayLabel")){
+                    dayLabel[count_day-1] = (Label) child;
+                    if(count_day < day_of_week){
+                        dayLabel[count_day-1].setText(String.valueOf(day++));
+                        dayBlock[count_day-1].setFill(Color.AZURE);
+                        day = (count_day+1==day_of_week) ? 1 : day;
+                    }
+                    else if(count_day < daysInMonth+day_of_week)
+                    {
+                       dayBlock[count_day-1].setFill(Color.WHITE);
+                       dayLabel[count_day-1].setText(String.valueOf(day++));
+                       day = (day-1==daysInMonth) ? 1 : day;
+                    }
+                    else
+                    {
+                       dayBlock[count_day-1].setFill(Color.AZURE);
+                       dayLabel[count_day-1].setText(String.valueOf(day++));
+                    }
+                    count_day++;
+                    //System.out.println(child);
+                }
+            }
+        }
+    }
+   
 }
--- SmartReminder/src/smartreminder/LoginPageController.java
+++ SmartReminder/src/smartreminder/LoginPageController.java
@@ -38,10 +38,10 @@ public class LoginPageController implements Initializable {
 
     @FXML
     private void click(ActionEvent event) {
-       SmartReminder.primaryStage.getScene().setRoot(SmartReminder.homePage);
+      // SmartReminder.primaryStage.getScene().setRoot(SmartReminder.homePage);
         
-        /*SmartReminder.primaryPane.getChildren().clear();
-        SmartReminder.primaryPane.getChildren().add(SmartReminder.loginForm);*/
+        SmartReminder.primaryPane.getChildren().clear();
+        SmartReminder.primaryPane.getChildren().add(SmartReminder.loginForm);
     }
 
 
